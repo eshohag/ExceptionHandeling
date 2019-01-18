5445b206733cfd5362d5addc1e24bb1b0e7dc82a
mdshohagmia <shohaghassan14@hotmail.com>
2019-01-18 15:00:32 +0600

    Upload

--- ExceptionHandeling/Program.cs
+++ ExceptionHandeling/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ExceptionHandeling
 {
@@ -10,6 +7,56 @@ namespace ExceptionHandeling
     {
         static void Main(string[] args)
         {
+            StreamReader aStreamReader = null;
+            try
+            {
+                try
+                {
+                    aStreamReader = new StreamReader(@"C:\Users\md-sh\OneDrive\Desktop\RiseUp\Day-8\ReadMe.txt");
+                    Console.WriteLine(aStreamReader.ReadToEnd());
+                }
+                catch (FileNotFoundException e)
+                {
+                    string writePath = @"C:\Users\md-sh\OneDrive\Desktop\RiseUp\Day-8\log2.txt";
+                    if (File.Exists(writePath))
+                    {
+                        StreamWriter aStreamWriter = new StreamWriter(writePath);
+                        aStreamWriter.Write(e.GetType().FullName);
+                        aStreamWriter.WriteLine();
+                        aStreamWriter.Write(e.Message);
+                        aStreamWriter.Close();
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException(writePath + " is not present");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    if (aStreamReader != null)
+                    {
+                        aStreamReader.Close();
+                    }
+                    Console.WriteLine("Finally Block");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Current Exception-{0}", e.GetType().FullName);
+                Console.WriteLine("Current Exception Message-{0}", e.Message);
+
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception-{0}", e.InnerException.GetType().Name);
+                    Console.WriteLine("Inner Exception Message-{0}", e.InnerException.Message);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
